582b82a8fdeda1ac8fa3a2c77442e01d385c2a92
Raheem-yash <57490275+Raheem-yash@users.noreply.github.com>
2019-11-21 16:18:26 +0500

    Add files via upload

---
+++ aex2.17.dev ch2.cpp
@@ -0,0 +1,12 @@
+/*(printing)write a program that prints the message "welcome to c++ programing" on one line with each word separated by one space. Do this several ways 
+a)using one statement with two stream insertion operators
+b)using one stream with four stream insertion operators
+c)using two statements with one stream insertion operator each */
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Wlcome to C++ programing ";
+	return 0;
+}
+
---
+++ d.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Welcome to C++ Programing"<<"welcome to C++ programing";
+	return 0;
+}
